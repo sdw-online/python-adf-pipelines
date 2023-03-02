6b8128a07478262fb49cb00d247ad3c9272aaf90
SDW <stephenoilesanmi@gmail.com>
2023-03-02 11:41:46 +0000

    attempting to authenticate Azure credentials using InteractiveBrowserCredential

--- adf_pipelines/s3_to_blob_pipeline.py
+++ adf_pipelines/s3_to_blob_pipeline.py
@@ -4,7 +4,7 @@ import boto3
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from dotenv import load_dotenv
 import os
-from azure.identity import DefaultAzureCredential
+from azure.identity import InteractiveBrowserCredential
 from azure.mgmt.datafactory import DataFactoryManagementClient
 from azure.mgmt.datafactory.models import LinkedServiceResource
 
@@ -50,28 +50,41 @@ DATA_FACTORY_NAME                   =   os.getenv("DATA_FACTORY_NAME")
 BLOB_STORAGE_ACCOUNT_NAME           =   os.getenv("BLOB_STORAGE_ACCOUNT_NAME")
 BLOB_STORAGE_ACCOUNT_KEY            =   os.getenv("BLOB_STORAGE_ACCOUNT_KEY")
 
-credentials                         =   DefaultAzureCredential()
+AUTHORITY_URL                       =   f'https://login.windows.net/{TENANT_ID}'
+
+
+credentials                         =   InteractiveBrowserCredential()
 blob_service_client                 =   BlobServiceClient(ACCOUNT_URL)
 adf_client                          =   DataFactoryManagementClient(credentials, SUBSCRIPTION_ID)
 
 data_factory                        =   adf_client.factories.get(RESOURCE_GROUP_NAME, DATA_FACTORY_NAME)
 
 
-blob_storage_linked_service_name = "azure_blob_storage_dest_001"
-blob_storage_connection_string = CONNECTION_STRING
+blob_storage_linked_service_name    =   os.getenv("LINKED_SERVICE_NAME_DEST")
+blob_storage_connection_string      =   CONNECTION_STRING
 
 
 # print(data_factory)
 
 
 
-# blob_storage_linked_service = LinkedServiceResource()
+blob_storage_linked_service = LinkedServiceResource(
+    type='AzureBlobStorage',
+    type_properties={
+        'connectionString': CONNECTION_STRING
+    }
+)
+
+
+blob_storage_linked_service_request = adf_client.linked_services.create_or_update(resource_group_name=RESOURCE_GROUP_NAME, 
+                                                                                   factory_name=DATA_FACTORY_NAME,
+                                                                                   linked_service_name=blob_storage_linked_service_name,
+                                                                                   linked_service=blob_storage_linked_service
+                                                                                   )
 
 
 
 
-
- 
 # Create an Azure credentials object 
 
 
